6a1573fa3039eb07d31325f912ca0fd4c55d3298
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-09-09 11:15:35 -0400

    navbar for homescreen

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+export default function Navbarr() {
+  return (
+    <Navbar bg="primary" expand="lg">
+      <Navbar.Brand href="#home">codecademy</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Catalog</Nav.Link>
+        <Nav.Link href="#link">pricing</Nav.Link>
+        <Nav.Link href="#link">For Business</Nav.Link>
+        <Nav.Link href="#link">Login</Nav.Link>
+        <Nav.Link href="#link">signup</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
